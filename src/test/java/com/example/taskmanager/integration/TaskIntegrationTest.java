364b56a7d08bbe02a2ec541f5ad80af9828b46cc
KemtaDonald <119060617+KemtaDonald@users.noreply.github.com>
2025-05-31 19:23:34 +0100

    Update TaskIntegrationTest.java

--- src/test/java/com/example/taskmanager/integration/TaskIntegrationTest.java
+++ src/test/java/com/example/taskmanager/integration/TaskIntegrationTest.java
@@ -2,6 +2,7 @@ package com.example.taskmanager.integration;
 
 import com.example.taskmanager.model.Task;
 import com.example.taskmanager.repository.TaskRepository;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
